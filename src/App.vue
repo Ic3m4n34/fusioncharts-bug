3fea411a9d2500dc8b00d0baac7b9ff87024fa1e
Nico Meyer <nico.meyer@i22.de>
2020-03-21 16:57:58 +0100

    shows bug

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,122 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <fusioncharts
+      :width="width"
+      :height="height"
+      :type="type"
+      :data-format="dataFormat"
+      :data-source="dataSource"
+    >
+    </fusioncharts>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import moment from 'moment';
+import FusionCharts from 'fusioncharts';
+
+import bars from '../sample-json-data/bars.json';
 
 export default {
   name: 'app',
-  components: {
-    HelloWorld
-  }
-}
+  computed: {
+    fusionChartData() {
+      const fusionDataStore = new FusionCharts.DataStore();
+      const fusionTable = fusionDataStore.createDataTable(this.getParsedBars(bars), this.schema);
+      return fusionTable;
+    },
+    parsedClips() {
+      return this.getParsedClips(bars);
+    },
+  },
+  data() {
+    return {
+      loading: false,
+      timer: null,
+      bars: [],
+      executions: [],
+
+      schema: [
+        {
+          name: "time",
+          type: "date",
+          format: "%m/%d/%Y, %I:%M:%S"
+        },
+        {
+          name: "price",
+          type: "number"
+        }
+      ],
+      width: "100%",
+      height: "1000",
+      type: "timeseries",
+      dataFormat: "json",
+      dataSource: {
+        data: this.fusionChartData,
+        dataMarker: null,
+        caption: {
+          text: "Open"
+        },
+        xAxis: {
+          clip: null
+        },
+        yAxis: {
+          referenceline: null,
+        },
+      }
+    };
+  },
+  mounted() {
+    this.dataSource.data = this.fusionChartData;
+    this.dataSource.xAxis.clip = this.parsedClips;
+  },
+
+  methods: {
+    async getItems() {
+      this.loading = true;
+     },
+     getParsedBars(bars) {
+      return bars.bars.map(bar => {
+        return [moment(bar.time).format("MM/DD/YYYY, hh:mm:ss"), bar.open];
+      });
+    },
+     getParsedClips(bars) {
+      return bars.clips.map(clip => {
+        return {
+          from: moment(clip.from).format('MM/DD/YYYY, hh:mm:ss'),
+          to: moment(clip.to).format('MM/DD/YYYY, hh:mm:ss'),
+          format: '%m/%d/%Y, %I:%M:%S',
+        }
+      });
+    },
+    getParsedDataMarkers(executions) {
+      return executions.map(execution => {
+        const executionType = execution.action;
+
+        return {
+          series: 'order',
+          time: moment(execution.filledOrCancelledAt).format('MM/DD/YYYY, hh:mm:ss'),
+          timeFormat: '%m/%d/%Y, %I:%M:%S',
+          identifier: executionType === 'BUY' ? 'B': 'S',
+        }
+      });
+    },
+    getParsedOrders(orders) {
+      return orders.map(order => {
+        return {
+          label: order.action,
+          value: order.limit,
+          style: {
+            marker: {
+              fill: order.action === 'SELL' ? '#fc0b03': '#3eb005',
+              stroke: order.action === 'SELL' ? '#fc0b03': '#3eb005',
+            },
+          },
+        };
+      });
+    },
+  },
+};
 </script>
 
 <style>
--- src/main.js
+++ src/main.js
@@ -1,6 +1,12 @@
 import Vue from 'vue'
 import App from './App.vue'
 
+import VueFusionCharts from 'vue-fusioncharts';
+import FusionCharts from 'fusioncharts';
+import TimeSeries from 'fusioncharts/fusioncharts.timeseries';
+
+Vue.use(VueFusionCharts, FusionCharts, TimeSeries);
+
 Vue.config.productionTip = false
 
 new Vue({
